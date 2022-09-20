a9ff40894a189e34fff24876f5ec331ba6eb0b76
charlesantunes <charleswcantunes@gmail.com>
2022-09-20 14:15:35 -0300

    configurando menu

--- src/Components/Menu/index.jsx
+++ src/Components/Menu/index.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { Container } from "./style";
+import { Container, Footer } from "./style";
 
 export default function Menu() {
     return(
         <>
         <Container>
             <ul>
-                <li><Link to="/" >Home</Link></li>
-                <li><Link to="/erro" >Error</Link></li>
+                <li><Link to="/" >ABOUT</Link></li>
+                <li><Link to="/erro" >RECIPES</Link></li>
+                <li>SUBSCRIBE</li>
             </ul>
         </Container>
+
+        
+        <Footer>
+            <ul>
+                <li><Link to="/" >ABOUT</Link></li>
+                <li><Link to="/erro" >RECIPES</Link></li>
+                <li>SUBSCRIBE</li>
+            </ul>
+        </Footer>
+
+
+
         </>
     )
 }
