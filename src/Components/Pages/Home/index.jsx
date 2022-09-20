ad2ee0ccd03d9cdcf1846f40e26e86bb63585601
charlesantunes <charleswcantunes@gmail.com>
2022-09-20 11:31:01 -0300

    primeiro comite com estrutura e pastas

---
+++ src/Components/Pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Menu from '../../Menu/index'
+
+
+
+export default function Home() {
+  return(
+    <>
+      <Menu />
+      <h1>Home</h1>
+    </>
+  )
+}
---
+++ src/Services/routes.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from '../Components/Pages/Home/index'
+import Error from '../Components/Pages/Error/index'
+
+
+export default function Rotas() {
+    return(
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/error" element={<Error />} />
+            </Routes>
+        </Router>
+    )
+}
