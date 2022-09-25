a39f334e4799199f61909107752e55bb5dd05c3d
charlesantunes <charleswcantunes@gmail.com>
2022-09-25 18:07:02 -0300

    terceiro - iniciando header

--- src/Components/Pages/Home/Header/index.jsx
+++ src/Components/Pages/Home/Header/index.jsx
@@ -1,23 +1,28 @@
 import React from "react";
 
 import * as S from "./style";
+//import Mascara2 from '../../../../Assets/Grupo de máscara 2.png'
+import RC from '../../../../Assets/RC.png'
 
 export default function HeaderFunc() {
   return(
     <>
       <S.Container>
         <S.Box>
-            <S.MenuLeft>
-              <h2>RC</h2>
-            </S.MenuLeft>
-            
-            <S.MenuRight>
-              <ul>
-                <li>ABOUT</li>
-                <li>RECIPES</li>
-                <li>SUBSCRIBE</li>
-              </ul>
-            </S.MenuRight>
+        <img src={RC} alt="imagem"/>
+          <S.BoxMenu>
+              <S.MenuLeft>
+                <h2>RC</h2>
+              </S.MenuLeft>
+              
+              <S.MenuRight>
+                <ul>
+                  <li>ABOUT</li>
+                  <li>RECIPES</li>
+                  <li>SUBSCRIBE</li>
+                </ul>
+              </S.MenuRight>
+          </S.BoxMenu>
         </S.Box>
       </S.Container>
     </>
--- src/Components/Pages/Home/Header/style.js
+++ src/Components/Pages/Home/Header/style.js
@@ -1,26 +1,48 @@
 import styled from "styled-components";
+import Mascara2 from '../../../../Assets/Grupo-de-máscara2.png'
+
 
 export const Container = styled.header`
     width: 100%;
-    height: 499px;
+    height: 864px;
     display: flex;
     justify-content: center;
     align-items: center;
 
     `
 export const Box = styled.div`
+    position: relative;
     border: solid 1px blue;
     width: 96%;
     height: 96%;
     display: flex;
-    justify-content: space-between;
-        a{
-            text-decoration: none;
+    justify-content: center;
+
+    background-image: url(${Mascara2});
+    background-size: 100%;
+    a{
+        text-decoration: none;
             
-            &:hover{
-                text-shadow: 0 0 10px red;
-                }
-        }    
+        &:hover{
+            text-shadow: 0 0 10px red;
+        }
+    }
+    img{
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        z-index:-1;
+    }
+    
+`
+
+export const BoxMenu = styled.div`
+border: solid green;
+height: 20vh;
+width: 90%;
+    display: flex;
+    justify-content: space-between;
+
 `
 export const MenuLeft = styled.div`
 border: solid 1px;
@@ -30,15 +52,15 @@ align-items: flex-end;
 `
 export const MenuRight = styled.div`
 border: solid 1px;
-width: 30%;
+width: 40%;
 height: 80px;
 display: flex;
 align-items: flex-end;
 ul{
-        display: flex;
-        justify-content: space-around;
-    }
-    li{
+    display: flex;
+    justify-content: space-around;
+}
+li{
         font-size: 1.5rem;
         &:hover{
         text-decoration: none;
