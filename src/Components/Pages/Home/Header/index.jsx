bc723d6483beafbb630a35e2ec75c79f4c8d1da9
drikadev <adriguti.adriana@hotmail.com>
2022-09-26 19:36:30 -0300

    Header finalizada e responsiva

--- src/Components/Pages/Home/Header/index.jsx
+++ src/Components/Pages/Home/Header/index.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 
 import * as S from "./style";
-//import Mascara2 from '../../../../Assets/Grupo de máscara 2.png'
 
 export default function HeaderFunc() {
-  return(
+  return (
     <>
       <S.Container>
         <S.Box>
-        {/* <img src={RC} alt="imagem"/> */}
-        <h1>RECIPES</h1>
+          
+          <div>
+            <figure></figure>
+          </div>
+          <h1>RECIPES</h1>
+
           <S.BoxMenu>
-              <S.MenuLeft>
-                <h2>RC</h2>
-              </S.MenuLeft>
-              
-              <S.MenuRight>
-                <ul>
-                  <li>ABOUT</li>
-                  <li>RECIPES</li>
-                  <li>SUBSCRIBE</li>
-                </ul>
-              </S.MenuRight>
+            <S.MenuLeft>
+              <h2>RC</h2>
+            </S.MenuLeft>
+
+            <S.MenuRight>
+              <ul>
+                <li>ABOUT</li>
+                <li>RECIPES</li>
+                <li>SUBSCRIBE</li>
+              </ul>
+            </S.MenuRight>
           </S.BoxMenu>
+
         </S.Box>
       </S.Container>
     </>
